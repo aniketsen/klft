65d0f7f478f7502b78352a2ac98b2550bd1c9e20
BenediktAuer <benedikt_auer01@web.de>
2025-05-07 15:35:24 +0200

    [Add] init & accessors

--- include/SpinorField.hpp
+++ include/SpinorField.hpp
@@ -22,11 +22,109 @@
 #pragma once
 #include "GLOBAL.hpp"
 #include "Tuner.hpp"
-
+// Nc number of colors
+// RepDim Dimension of Gamma matrices, Nd = RepDim
 namespace klft {
-template <size_t Nc, size_t Nd>
+template <size_t Nc, size_t RepDim>
 struct deviceSpinorField {
   deviceSpinorField() = delete;
+
+  // initialize all sites to a given value
+
+  deviceSpinorField(const index_t L0, const index_t L1, const index_t L2,
+                    const index_t L3, const complex_t init)
+      : dimensions({L0, L1, L2, L3}) {
+    do_init(L0, L1, L2, L3, field, init);
+  }
+
+  deviceSpinorField(const IndexArray<4> &dimensions, const complex_t init)
+      : dimensions(dimensions) {
+    do_init(dimensions[0], dimensions[1], dimensions[2], dimensions[3], field,
+            init);
+  }
+
+  // initialize all sites to given Spinor
+  deviceSpinorField(const index_t L0, const index_t L1, const index_t L2,
+                    const index_t L3, const Spinor<Nc, RepDim> &init)
+      : dimensions({L0, L1, L2, L3}) {
+    do_init(L0, L1, L2, L3, field, init);
+  }
+  deviceSpinorField(const IndexArray<4> &dimensions,
+                    const Spinor<Nc, RepDim> &init)
+      : dimensions(dimensions) {
+    do_init(dimensions[0], dimensions[1], dimensions[2], dimensions[3], field,
+            init);
+  }
+
+  void do_init(const index_t L0, const index_t L1, const index_t L2,
+               const index_t L3, SpinorField<Nc, RepDim> &V,
+               const complex_t init) {
+    if (KLFT_VERBOSITY > 5) {
+      printf("Initialize SpinorField with Single Value");
+    }
+    Kokkos::realloc(Kokkos::WithoutInitializing, V, L0, L1, L2, L3);
+    tune_and_launch_for<4>(
+        "init_deviceSpinorField", IndexArray<4>{0, 0, 0, 0},
+        IndexArray<4>{L0, L1, L2, L3},
+        KOKKOS_LAMBDA(const index_t i0, const index_t i1, const index_t i2,
+                      const index_t i3) {
+#pragma unroll
+          for (index_t c1 = 0; c1 < Nc; ++c1) {
+#pragma unroll
+            for (index_t c2 = 0; c2 < RepDim; ++c2) {
+              V(i0, i1, i2, i3)[c1][c2] = init;
+            }
+          }
+        });
+    Kokkos::fence();
+  }
+  void do_init(const index_t L0, const index_t L1, const index_t L2,
+               const index_t L3, SpinorField<Nc, RepDim> &V,
+               const Spinor<Nc, RepDim> &init) {
+    if (KLFT_VERBOSITY > 5) {
+      printf("Initialize SpinorField with Spinors");
+    }
+    Kokkos::realloc(Kokkos::WithoutInitializing, V, L0, L1, L2, L3);
+    tune_and_launch_for<4>(
+        "init_deviceSpinorField", IndexArray<4>{0, 0, 0, 0},
+        IndexArray<4>{L0, L1, L2, L3},
+        KOKKOS_LAMBDA(const index_t i0, const index_t i1, const index_t i2,
+                      const index_t i3) { V(i0, i1, i2, i3) = init; });
+    Kokkos::fence();
+  }
+
+  SpinorField<Nc, RepDim> field;
+  const IndexArray<4> dimensions;
+
+  // define accessors
+  template <typename indexType>
+  KOKKOS_FORCEINLINE_FUNCTION SUN<Nc> &operator()(const indexType i0,
+                                                  const indexType i1,
+                                                  const indexType i2,
+                                                  const indexType i3) const {
+    return field(i0, i1, i2, i3);
+  }
+
+  template <typename indexType>
+  KOKKOS_FORCEINLINE_FUNCTION SUN<Nc> &operator()(const indexType i0,
+                                                  const indexType i1,
+                                                  const indexType i2,
+                                                  const indexType i3) {
+    return field(i0, i1, i2, i3);
+  }
+
+  // define accessors with 4D Kokkos array
+  template <typename indexType>
+  KOKKOS_FORCEINLINE_FUNCTION SUN<Nc> &operator()(
+      const Kokkos::Array<indexType, 4> site) const {
+    return field(site[0], site[1], site[2], site[3]);
+  }
+
+  template <typename indexType>
+  KOKKOS_FORCEINLINE_FUNCTION SUN<Nc> &operator()(
+      const Kokkos::Array<indexType, 4> site) {
+    return field(site[0], site[1], site[2], site[3]);
+  }
 };
 
 }  // namespace klft
