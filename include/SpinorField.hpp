dbecdbc8b896e8fc51ce8ce0d868a0477753ddeb
BenediktAuer <benedikt_auer01@web.de>
2025-05-07 13:16:22 +0200

    [Add] Initial Spinor Field type

---
+++ include/SpinorField.hpp
@@ -0,0 +1,32 @@
+//******************************************************************************/
+//
+// This file is part of the Kokkos Lattice Field Theory (KLFT) library.
+//
+// KLFT is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// KLFT is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with KLFT.  If not, see <http://www.gnu.org/licenses/>.
+//
+//******************************************************************************/
+
+// define structs for initializing SUN field
+
+#pragma once
+#include "GLOBAL.hpp"
+#include "Tuner.hpp"
+
+namespace klft {
+template <size_t Nc, size_t Nd>
+struct deviceSpinorField {
+  deviceSpinorField() = delete;
+};
+
+}  // namespace klft
